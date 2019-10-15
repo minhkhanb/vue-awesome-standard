285c7a6adf3173ef078cc3924bf42728db87baa9
Kha Le <minhkhanb@gmail.com>
2019-10-15 11:01:44 -0700

    npm update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,7 +15,7 @@
 
       <v-flex mb-4>
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
+          new message
         </h1>
         <p class="subheading font-weight-regular">
           For help and collaboration with other Vuetify developers,
@@ -86,6 +86,7 @@
 
 <script>
 export default {
+  name: 'HelloWorld',
   data: () => ({
     ecosystem: [
       {
--- tests/unit/components/HelloWorld.spec.js
+++ tests/unit/components/HelloWorld.spec.js
@@ -1,12 +1,17 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
 import { shallowMount } from '@vue/test-utils';
 import HelloWorld from '~/components/HelloWorld.vue';
+
+Vue.use(Vuetify);
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message';
     const wrapper = shallowMount(HelloWorld, {
+      name: 'HelloWorld',
       propsData: { msg },
     });
-    expect(wrapper.text()).toMatch(msg);
+    expect(wrapper.find('h1').text()).toMatch(msg);
   });
 });
